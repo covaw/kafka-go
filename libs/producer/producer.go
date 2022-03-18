7cd936f9905457548a2e74456162a6d069a2d53f
William Cova <wcova@andreani.com>
2022-03-18 12:43:45 -0300

    log bin for value kafka

--- libs/producer/producer.go
+++ libs/producer/producer.go
@@ -71,6 +71,8 @@ func Producer(
 		fmt.Println(index)
 	}
 
+	fmt.Println(bin)
+
 	p.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Value:          bin,
