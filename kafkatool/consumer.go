c1d4e34ede68c633306ea317fac6a438c1237064
William Cova <wcova@andreani.com>
2022-04-12 18:11:33 -0300

    upload consumer

--- kafkatool/consumer.go
+++ kafkatool/consumer.go
@@ -9,6 +9,11 @@ import (
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/linkedin/goavro/v2"
+	"github.com/mitchellh/mapstructure"
+)
+
+var (
+	_config *kafka.ConfigMap
 )
 
 func Consumer[K any](
@@ -101,4 +106,30 @@ func Consumer[K any](
 	fmt.Println(fmt.Sprintf("%s", decoded))
 
 	return decoded
+}
+
+func AddKafka(configuration map[string]interface{}, provider string) {
+	if len(provider) == 0 {
+		provider := "Kafka"
+		fmt.Println(provider)
+	}
+
+	var configurations = make(map[string]string)
+	mapstructure.Decode(configuration[provider], &configurations)
+	kafkaConfig := &kafka.ConfigMap{
+		"bootstrap.servers":        configurations["broker"],
+		"broker.address.family":    "v4",
+		"group.id":                 configurations["group"],
+		"session.timeout.ms":       6000,
+		"security.protocol":        configurations["protocol"],
+		"auto.offset.reset":        "earliest",
+		"ssl.certificate.location": configurations["certificate"]}
+	fmt.Println(kafkaConfig)
+	_config := kafkaConfig
+
+	fmt.Println("Kafka-Config:", _config)
+}
+
+func GetConfig() *kafka.ConfigMap {
+	return _config
 }
--- kafkatool/setting.go
+++ kafkatool/setting.go
@@ -1,38 +1,38 @@
 package kafkatool
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"github.com/mitchellh/mapstructure"
-)
+// 	"github.com/confluentinc/confluent-kafka-go/kafka"
+// 	"github.com/mitchellh/mapstructure"
+// )
 
-var (
-	_config *kafka.ConfigMap
-)
+// var (
+// 	_config *kafka.ConfigMap
+// )
 
-func AddKafka(configuration map[string]interface{}, provider string) {
-	if len(provider) == 0 {
-		provider := "Kafka"
-		fmt.Println(provider)
-	}
+// func AddKafka(configuration map[string]interface{}, provider string) {
+// 	if len(provider) == 0 {
+// 		provider := "Kafka"
+// 		fmt.Println(provider)
+// 	}
 
-	var configurations = make(map[string]string)
-	mapstructure.Decode(configuration[provider], &configurations)
-	kafkaConfig := &kafka.ConfigMap{
-		"bootstrap.servers":        configurations["broker"],
-		"broker.address.family":    "v4",
-		"group.id":                 configurations["group"],
-		"session.timeout.ms":       6000,
-		"security.protocol":        configurations["protocol"],
-		"auto.offset.reset":        "earliest",
-		"ssl.certificate.location": configurations["certificate"]}
-	fmt.Println(kafkaConfig)
-	_config := kafkaConfig
+// 	var configurations = make(map[string]string)
+// 	mapstructure.Decode(configuration[provider], &configurations)
+// 	kafkaConfig := &kafka.ConfigMap{
+// 		"bootstrap.servers":        configurations["broker"],
+// 		"broker.address.family":    "v4",
+// 		"group.id":                 configurations["group"],
+// 		"session.timeout.ms":       6000,
+// 		"security.protocol":        configurations["protocol"],
+// 		"auto.offset.reset":        "earliest",
+// 		"ssl.certificate.location": configurations["certificate"]}
+// 	fmt.Println(kafkaConfig)
+// 	_config := kafkaConfig
 
-	fmt.Println("Kafka-Config:", _config)
-}
+// 	fmt.Println("Kafka-Config:", _config)
+// }
 
-func GetConfig() *kafka.ConfigMap {
-	return _config
-}
+// func GetConfig() *kafka.ConfigMap {
+// 	return _config
+// }
